d9e58b4a904f48a08216857644a92608ed51b29d
tgrifka <grifka94@yahoo.com>
2019-12-17 15:01:08 -0500

    Fixed somethings and commented code, almost done with the server and client

--- src/server/AdventureClient.java
+++ src/server/AdventureClient.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.net.SocketException;
 
 
 public class AdventureClient {
